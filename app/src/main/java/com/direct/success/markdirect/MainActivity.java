c36f184d81d70bb5ef160f60034a083474f79eeb
Antonio Jesus Cazalla <gemabeltran@Air-de-Gema.Home>
2017-02-27 13:12:34 +0100

    Added libraries and Facebook button

--- app/src/main/java/com/direct/success/markdirect/MainActivity.java
+++ app/src/main/java/com/direct/success/markdirect/MainActivity.java
@@ -3,15 +3,18 @@ package com.direct.success.markdirect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Intent i = new Intent(MainActivity.this, GeneralActivity.class);
-        //startActivity(i);
 
 
     }
