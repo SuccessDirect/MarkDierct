f7e650a2cb380297f07605dddda8e7c8f696eb0f
jmajyo-por <jmajyo@gmail.com>
2017-03-02 12:19:39 +0100

    added code for enabled/disabled notifications

--- app/src/main/java/com/direct/success/markdirect/fragments/SettingsFragment.java
+++ app/src/main/java/com/direct/success/markdirect/fragments/SettingsFragment.java
@@ -1,7 +1,10 @@
 package com.direct.success.markdirect.fragments;
 
 
+import android.app.Notification;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,10 +14,14 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import com.direct.success.markdirect.R;
+import com.direct.success.markdirect.utils.Notifications;
+
+import io.realm.Realm;
 
 public class SettingsFragment extends Fragment {
-private Switch notificationSwitchButton;
-private Switch bluetoothSwitchButton;
+    private Switch notificationSwitchButton;
+    private Switch bluetoothSwitchButton;
+    private Notifications notification;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -35,13 +42,33 @@ private Switch bluetoothSwitchButton;
         }else{
             bluetoothSwitchButton.setChecked(false);
         }
+        SharedPreferences prefs = getContext().getSharedPreferences("Preferences",Context.MODE_PRIVATE);
+        if(prefs.getBoolean("Notification",true))
+        {
+            notificationSwitchButton.setChecked(true);
+        }else{
+            notificationSwitchButton.setChecked(false);
+        }
         notificationSwitchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
                     //Cuando este activo y pulso las desactivo
+                    SharedPreferences prefs =
+                            getContext().getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putBoolean("Notification",false);
+                    editor.commit();
+
                 }else{
                     //Cuando este apagado y pulso las activo
+                    SharedPreferences prefs =
+                            getContext().getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putBoolean("Notification",true);
+                    editor.commit();
                 }
             }
         });
--- app/src/main/java/com/direct/success/markdirect/utils/Notifications.java
+++ app/src/main/java/com/direct/success/markdirect/utils/Notifications.java
@@ -8,7 +8,21 @@ import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 
+import io.realm.RealmObject;
+
 public class Notifications {
+    private boolean activo;
+    public Notifications() {
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
     public static void postNotification(Context context, Class<? extends Activity> activity, String titulo, String texto, int icono, int color, int identificator) {
         Intent i = new Intent(context, activity);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 5, i, 0);
