8047bcd77680e0cd58e3e50bd858e68eac6ee53f
Markinhos_3\Markinhos3 <hablamos@marcosvaldi.com>
2017-03-06 10:55:24 +0100

    chaged_colors_mainToolbar_logo_loginToolbar

--- app/src/main/java/com/direct/success/markdirect/activities/LoginActivity.java
+++ app/src/main/java/com/direct/success/markdirect/activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -31,6 +32,10 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.toolbar_login_activity);
+
         setContentView(R.layout.activity_login);
 
         SharedPreferences prefs =
