95ef69d921bf881171312a20bc777b810bd6ecdc
Antonio Jesus Cazalla <gemabeltran@MacBook-Air-de-Gema.local>
2017-03-06 18:27:21 +0100

    Added register design

--- app/src/main/java/com/direct/success/markdirect/activities/RegisterActivity.java
+++ app/src/main/java/com/direct/success/markdirect/activities/RegisterActivity.java
@@ -1,13 +1,58 @@
 package com.direct.success.markdirect.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+
+import com.direct.success.markdirect.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Spinner cmbOpciones;
+    private ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        final String[] datos =
+                new String[]{"Hombre","Mujer"};
+
+        ArrayAdapter<String> adaptador =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_spinner_item, datos);
+
+        scrollView = (ScrollView) findViewById(R.id.activity_register);
+        cmbOpciones = (Spinner)findViewById(R.id.CmbOpciones);
+
+
+        adaptador.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        cmbOpciones.setAdapter(adaptador);
+
+        cmbOpciones.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent,
+                                               android.view.View v, int position, long id) {
+                        //lblMensaje.setText("Seleccionado: " +
+                          //      parent.getItemAtPosition(position));
+                    }
+
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        //lblMensaje.setText("");
+                    }
+                });
+
+    }
+
+    public void quitKeyboard(View v){
+        InputMethodManager imm =
+                (InputMethodManager) v.getContext().getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 }
