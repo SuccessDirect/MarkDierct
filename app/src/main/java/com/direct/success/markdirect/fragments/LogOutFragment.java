a7c87e4160188ab05a6584a581ecc11a35b8957c
Antonio Jesus Cazalla <gemabeltran@MacBook-Air-de-Gema.local>
2017-03-07 13:27:17 +0100

    Added button in logOut activity for manual login

--- app/src/main/java/com/direct/success/markdirect/fragments/LogOutFragment.java
+++ app/src/main/java/com/direct/success/markdirect/fragments/LogOutFragment.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.direct.success.markdirect.R;
 import com.direct.success.markdirect.activities.LoginActivity;
@@ -22,6 +23,7 @@ import com.facebook.login.widget.LoginButton;
 public class LogOutFragment extends Fragment {
 
     private LoginButton loginButton;
+    private Button button;
 
     public LogOutFragment() {
         // Required empty public constructor
@@ -35,7 +37,37 @@ public class LogOutFragment extends Fragment {
         View view =inflater.inflate(R.layout.fragment_log_out, container, false);
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Log Out");
 
-        loginButton = (LoginButton) view.findViewById(R.id.loo_out_login_button_);
+        button = (Button) view.findViewById(R.id.exit_button);
+
+        loginButton = (LoginButton) view.findViewById(R.id.log_out_login_button_);
+
+        SharedPreferences prefs =
+                getContext().getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
+        String token = prefs.getString("Tipo", null);
+
+        if (token.equals("FACEBOOK")){
+            button.setVisibility(View.GONE);
+        }else {
+            loginButton.setVisibility(View.GONE);
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences prefs =
+                        v.getContext().getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("Token", null);
+                editor.putString("Tipo", null);
+                editor.commit();
+                LoginManager.getInstance().logOut();
+                Intent i = new Intent(getContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +84,7 @@ public class LogOutFragment extends Fragment {
 
             }
         });
+
 
         return view;
     }
